29647e8965fa858c93fb7f8e6bf1d16bbf9321d0
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-07-29 14:13:20 +0200

    cm

--- benchmark.py
+++ benchmark.py
@@ -10,7 +10,7 @@ benchmark_options = [
 bsizes = [2, 4, 16, 64, 256, 512, 1024, 2048]
 
 for bsize in bsizes:
-    stream = os.popen(base_command + '--threads 1 --bsize ' + bsize + ' --op write --mode rand --lib libaio')
+    stream = os.popen(base_command + '--threads 1 --bsize ' + str(bsize) + ' --op write --mode rand --lib libaio')
     output.append(stream.readline())
 
 
