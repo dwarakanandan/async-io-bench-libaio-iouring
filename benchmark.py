96cf49f662cad7066013855019987413e6417027
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-07-29 14:24:32 +0200

    cm

--- benchmark.py
+++ benchmark.py
@@ -2,13 +2,30 @@ import os
 
 base_command = 'sudo ./build/benchmark --file /dev/md0 '
 
-bsizes = [2, 4, 16, 64, 256, 512, 1024, 2048]
+bsizes = [2, 16, 64, 256, 512, 1024, 2048]
+
+def runBenchmark(lib):
+    for bsize in bsizes:
+        stream = os.popen(base_command + '--threads 1 --bsize ' + str(bsize) + ' --op read --mode seq --lib ' + lib)
+        outputs.append(stream.readline().strip())
+
+    for bsize in bsizes:
+        stream = os.popen(base_command + '--threads 1 --bsize ' + str(bsize) + ' --op read --mode rand --lib' + lib)
+        outputs.append(stream.readline().strip())
+
+    for bsize in bsizes:
+        stream = os.popen(base_command + '--threads 1 --bsize ' + str(bsize) + ' --op write --mode seq --lib' + lib)
+        outputs.append(stream.readline().strip())
+
+    for bsize in bsizes:
+        stream = os.popen(base_command + '--threads 1 --bsize ' + str(bsize) + ' --op write --mode rand --lib' + lib)
+        outputs.append(stream.readline().strip())
+
 
 outputs = []
-for bsize in bsizes:
-    stream = os.popen(base_command + '--threads 1 --bsize ' + str(bsize) + ' --op write --mode rand --lib libaio')
-    outputs.append(stream.readline())
 
+runBenchmark('syncio')
+runBenchmark('libaio')
 
 for output in outputs:
     print(output)
