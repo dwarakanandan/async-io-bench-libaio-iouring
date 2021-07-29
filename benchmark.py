218c8c64dce8f76929b51101cddc542f1b6671ea
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-07-29 14:15:01 +0200

    cm

--- benchmark.py
+++ benchmark.py
@@ -1,17 +1,14 @@
 import os
 
 base_command = 'sudo ./build/benchmark --file /dev/md0 '
-output = []
-
-benchmark_options = [
-    '--threads 1 --bsize 2 --op write --mode rand --lib libaio'
-]
 
 bsizes = [2, 4, 16, 64, 256, 512, 1024, 2048]
 
+outputs = []
 for bsize in bsizes:
     stream = os.popen(base_command + '--threads 1 --bsize ' + str(bsize) + ' --op write --mode rand --lib libaio')
-    output.append(stream.readline())
+    outputs.append(stream.readline())
 
 
-print(output)
+for output in outputs:
+    print(output)
