bfeed4834fbd76a3dcb040911200ddcf28db5212
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-07-29 14:04:43 +0200

    cm

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,6 @@
 import os
 
-base_command = 'sudo ./build/benchmark --file /dev/md0'
+base_command = 'sudo ./build/benchmark --file /dev/md0 '
 
 stream = os.popen(base_command + '--threads 8 --bsize 2 --op write --mode rand --lib libaio')
 output = stream.read()
