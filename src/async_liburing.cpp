963286e7a243ae0df5221aa1517ee7ab2a24d896
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-08-06 15:39:57 +0200

    cm

--- src/async_liburing.cpp
+++ src/async_liburing.cpp
@@ -2,56 +2,6 @@
 
 using namespace std;
 
-Result_t async_liburing_temp(const RuntimeArgs_t& args) {
-    size_t buffer_size = 1024 * args.blk_size;
-
-    char* buffer = (char *) aligned_alloc(1024, buffer_size);
-	memset(buffer, '0', buffer_size);
-
-    struct io_uring ring;
-    io_uring_queue_init(QUEUE_DEPTH, &ring, 0);
-
-    int blocks = 1;
-
-    struct file_info *fi = (file_info*) malloc(sizeof(*fi) + (sizeof(struct iovec) * blocks));
-
-    fi->iovecs[0].iov_len = 1024;
-    fi->iovecs[0].iov_base = buffer;
-
-    fi->file_sz = 1024;
-
-    /* Get an SQE */
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
-    /* Setup a readv operation */
-    io_uring_prep_readv(sqe, args.fd, fi->iovecs, blocks, 1024);
-    /* Set user data */
-    io_uring_sqe_set_data(sqe, fi);
-    /* Finally, submit the request */
-    io_uring_submit(&ring);
-
-    struct io_uring_cqe *cqe;
-    int ret = io_uring_wait_cqe(&ring, &cqe);
-        if (ret < 0) {
-        perror("io_uring_wait_cqe");
-        return return_error();
-    }
-    if (cqe->res < 0) {
-        fprintf(stderr, "Async readv failed.\n");
-        return return_error();
-    }
-
-    fi = (file_info*) io_uring_cqe_get_data(cqe);
-
-    cout << fi->file_sz << std::endl;
-    char* obuf = (char*) fi->iovecs[0].iov_base;
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d=%d:%c\n", i, obuf[i], obuf[i]);
-    }
-    
-    return return_error();
-}
-
 Result_t async_liburing(const RuntimeArgs_t& args) {
     size_t buffer_size = 1024 * args.blk_size;
     uint64_t ops_submitted = 0, ops_returned = 0;
@@ -68,7 +18,6 @@ Result_t async_liburing(const RuntimeArgs_t& args) {
         fi->iovecs[i].iov_base = buffer;
     }
 
-    cout << "starting bkar" << endl;
 	double start = getTime();
 	while (getTime() - start < RUN_TIME) {
         off_t offset =  args.read_offset + (buffer_size * args.oio * ops_submitted);
@@ -96,7 +45,7 @@ Result_t async_liburing(const RuntimeArgs_t& args) {
 
         ops_returned+= args.oio;
     }
-    
+    cout << "got results" << endl;
     Result_t results;
     results.throughput = calculateThroughputGbps(ops_returned, buffer_size);
     results.op_count = ops_returned;
