1c1825f53d7cb52f3077e603196e59336a00ed10
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-08-12 21:22:45 +0200

    cm

--- src/async_liburing.cpp
+++ src/async_liburing.cpp
@@ -14,6 +14,7 @@ Result_t async_liburing_read(const RuntimeArgs_t& args) {
     __kernel_timespec timeout;
     sigset_t sigset;
     char* buffer[args.oio];
+    off_t offsets[args.oio];
     int ret;
 
     iovecs = (iovec*) calloc(args.oio, sizeof(struct iovec));
@@ -21,6 +22,9 @@ Result_t async_liburing_read(const RuntimeArgs_t& args) {
     {
         buffer[i] = (char *) aligned_alloc(1024, buffer_size);
 	    memset(buffer[i], '0', buffer_size);
+
+        offsets[i] = getOffset(args.read_offset, buffer_size, i, isRand);
+
         iovecs[i].iov_base = buffer[i];
         iovecs[i].iov_len = buffer_size;
     }
@@ -42,8 +46,7 @@ Result_t async_liburing_read(const RuntimeArgs_t& args) {
                 fprintf(stderr, "io_uring_get_sqe failed\n");
                 return return_error();
             }
-            off_t offset = getOffset(args.read_offset, buffer_size, ops_submitted+i, isRand);
-            io_uring_prep_readv(sqe, args.fd, &iovecs[i], 1, offset);
+            io_uring_prep_readv(sqe, args.fd, &iovecs[i], 1, offsets[i]);
         }
 
         /* Submit args.oio operations */
@@ -80,76 +83,7 @@ Result_t async_liburing_read(const RuntimeArgs_t& args) {
 }
 
 Result_t async_liburing_write(const RuntimeArgs_t& args) {
-    size_t buffer_size = 1024 * args.blk_size;
-    uint64_t ops_submitted = 0, ops_returned = 0, ops_failed = 0;
-	bool isRand = (args.opmode == RANDOM);
-
-    struct io_uring ring;
-    struct io_uring_sqe *sqe;
-    struct io_uring_cqe *cqe;
-    struct iovec *iovecs;
-    __kernel_timespec timeout;
-    char* buffer[args.oio];
-    int ret;
-
-    iovecs = (iovec*) calloc(args.oio, sizeof(struct iovec));
-    for (int i = 0; i < args.oio; i++)
-    {
-        buffer[i] = (char *) aligned_alloc(1024, buffer_size);
-	    memset(buffer[i], '0', buffer_size);
-        iovecs[i].iov_base = buffer[i];
-        iovecs[i].iov_len = buffer_size;
-    }
-
-    /* Initialize io_uring */
-    ret = io_uring_queue_init(1024, &ring, 0);
-    if (ret < 0) {
-        perror(getErrorMessageWithTid(args, "io_uring_queue_init"));
-        return return_error();
-    }
-
-    double start = getTime();
-	while (getTime() - start < RUN_TIME) {
-        /* Get a Submission Queue Entry */
-        sqe = io_uring_get_sqe(&ring);
-        if (sqe == NULL) {
-            fprintf(stderr, "io_uring_get_sqe failed\n");
-            return return_error();
-        }
-        //off_t offset = getOffset(args.read_offset, buffer_size, ops_submitted, isRand);
-        io_uring_prep_writev(sqe, args.fd, iovecs, args.oio, args.read_offset);
-
-        /* Submit args.oio operations */
-        ret = io_uring_submit(&ring);
-        ops_submitted+= args.oio;
-
-        /* Wait for IO request to complete */
-        timeout.tv_sec = 1;
-        ret = io_uring_wait_cqe_timeout(&ring, &cqe, &timeout);
-        if (ret < 0) {
-            perror(getErrorMessageWithTid(args, "io_uring_wait_cqe"));
-            return return_error();
-        }
-
-        /* Check completion event result code */
-        if (cqe->res < 0) {
-            ops_failed+= args.oio;
-        }
-        io_uring_cqe_seen(&ring, cqe);
-
-        ops_returned+= args.oio;
-    }
-
-    /* Cleanup io_uring */
-    io_uring_queue_exit(&ring);
-
-    Result_t results;
-    results.throughput = calculateThroughputGbps(ops_returned-ops_failed, buffer_size);
-    results.op_count = ops_returned-ops_failed;
-	results.ops_submitted = ops_submitted;
-	results.ops_returned = ops_returned;
-	results.ops_failed = ops_failed;
-    return results;
+    return return_error();
 }
 
 Result_t async_liburing(const RuntimeArgs_t& args) {
