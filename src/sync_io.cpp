ea45187fe80c2bcddbb1e34e935a03a9069c71e3
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-08-12 20:44:35 +0200

    updated syncio

--- src/sync_io.cpp
+++ src/sync_io.cpp
@@ -8,21 +8,30 @@ Result_t syncio(const RuntimeArgs_t& args) {
     bool isRead = (args.operation == READ);
     bool isRand = (args.opmode == RANDOM);
 
-    char* buffer = (char *) aligned_alloc(1024, buffer_size);
-    memset(buffer, '0', buffer_size);
+    char* buffer[args.oio];
+
+    struct iovec *iovecs;
+    iovecs = (iovec*) calloc(args.oio, sizeof(struct iovec));
+    for (int i = 0; i < args.oio; i++)
+    {
+        buffer[i] = (char *) aligned_alloc(1024, buffer_size);
+	    memset(buffer[i], '0', buffer_size);
+        iovecs[i].iov_base = buffer[i];
+        iovecs[i].iov_len = buffer_size;
+    }
 
     double start = getTime();
     while (getTime() - start < RUN_TIME) {
         ssize_t opCount = isRead ?
-            pread(args.fd, buffer, buffer_size, getOffset(args.read_offset, buffer_size, ops, isRand)) :
-            pwrite(args.fd, buffer, buffer_size, getOffset(args.read_offset, buffer_size, ops, isRand));
+            preadv(args.fd, iovecs, args.oio, getOffset(args.read_offset, buffer_size, ops, isRand)) :
+            pwritev(args.fd, iovecs, args.oio, getOffset(args.read_offset, buffer_size, ops, isRand));
 
         if(opCount < 0) {
             perror("IO error");
             return return_error();
         }
 
-        ops++;
+        ops+= opCount;
     }
 
     Result_t results;
--- src/sync_io.h
+++ src/sync_io.h
@@ -1,3 +1,4 @@
 #include "helper.h"
+#include <sys/uio.h>
 
 Result_t syncio(const RuntimeArgs_t& args);
