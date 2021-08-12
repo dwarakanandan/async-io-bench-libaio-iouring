8b103afe77b4b51a2fbd095816f120f1ea633553
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-08-12 22:06:57 +0200

    cm

--- src/sync_io.cpp
+++ src/sync_io.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 Result_t _syncio_read(const RuntimeArgs_t& args) {
-    uint64_t ops = 0;
+    uint64_t ops_submitted = 0, ops_returned = 0, ops_failed = 0;
     size_t buffer_size = 1024 * args.blk_size;
     bool isRand = (args.opmode == RANDOM);
 
@@ -12,23 +12,25 @@ Result_t _syncio_read(const RuntimeArgs_t& args) {
 
     double start = getTime();
     while (getTime() - start < RUN_TIME) {
-        ssize_t opCount = pread(args.fd, buffer, buffer_size, getOffset(args.read_offset, buffer_size, ops, isRand));
+        ssize_t opCount = pread(args.fd, buffer, buffer_size, getOffset(args.read_offset, buffer_size, ops_submitted, isRand));
+        ops_submitted++;
         if(opCount < 0) {
-            perror("Read error");
-            return return_error();
+            ops_failed++;
         }
-
-        ops++;
+        ops_returned++;
     }
 
     Result_t results;
-    results.throughput = calculateThroughputGbps(ops, buffer_size);
-    results.op_count = ops;
+    results.throughput = calculateThroughputGbps(ops_returned-ops_failed, buffer_size);
+    results.op_count = ops_returned-ops_failed;
+	results.ops_submitted = ops_submitted;
+	results.ops_returned = ops_returned;
+	results.ops_failed = ops_failed;
     return results;
 }
 
 Result_t _syncio_write(const RuntimeArgs_t& args) {
-    uint64_t ops = 0;
+    uint64_t ops_submitted = 0, ops_returned = 0, ops_failed = 0;
     size_t buffer_size = 1024 * args.blk_size;
     bool isRand = (args.opmode == RANDOM);
 
@@ -37,18 +39,20 @@ Result_t _syncio_write(const RuntimeArgs_t& args) {
 
     double start = getTime();
     while (getTime() - start < RUN_TIME) {
-        ssize_t opCount = pwrite(args.fd, buffer, buffer_size, getOffset(args.read_offset, buffer_size, ops, isRand));
+        ssize_t opCount = pwrite(args.fd, buffer, buffer_size, getOffset(args.read_offset, buffer_size, ops_submitted, isRand));
+        ops_submitted++;
         if(opCount < 0) {
-            perror("Write error");
-            return return_error();
+            ops_failed++;
         }
-
-        ops++;
+        ops_returned++;
     }
 
     Result_t results;
-    results.throughput = calculateThroughputGbps(ops, buffer_size);
-    results.op_count = ops;
+    results.throughput = calculateThroughputGbps(ops_returned-ops_failed, buffer_size);
+    results.op_count = ops_returned-ops_failed;
+	results.ops_submitted = ops_submitted;
+	results.ops_returned = ops_returned;
+	results.ops_failed = ops_failed;
     return results;
 }
 
